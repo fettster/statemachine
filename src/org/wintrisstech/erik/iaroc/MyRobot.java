670a6cbe1574a44f64a89d6a8a9e5f70e89cafca
droid2 <droid2@10.0.1.6>
2012-04-25 18:06:39 -0700

    William, James, and Savera

--- src/org/wintrisstech/erik/iaroc/MyRobot.java
+++ src/org/wintrisstech/erik/iaroc/MyRobot.java
@@ -1,21 +1,119 @@
 package org.wintrisstech.erik.iaroc;
 
+import android.os.SystemClock;
 import ioio.lib.api.IOIO;
 import ioio.lib.api.exception.ConnectionLostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.wintrisstech.irobot.ioio.IRobotCreateInterface;
 
-
-public class MyRobot extends Ferrari 
+public class MyRobot extends Ferrari
 {
+    private int[][] stateTable =
+    {
+        {
+            0, 2, 1, 3
+        },
+        {
+            1, 2, 1, 3
+        },
+        {
+            2, 2, 1, 3
+        },
+        {
+            3, 2, 1, 3
+        }
+            
+    };
+    private int presentState = 0;
+    private int statePointer = 0;
+   
+    
 
     MyRobot(IOIO ioio, IRobotCreateInterface iRobotCreate, Dashboard dashboard) throws ConnectionLostException
     {
         super(ioio, iRobotCreate, dashboard);
     }
 
+    /*
+     * THE FUN STARTS HERE!
+     */
     public void goRobot()
     {
-        dashboard.log("hello from My Robot");
+        try
+        {
+            driveDirect(100, 100);
+            stateControler();
+        } catch (Exception ex)
+        {
+            Logger.getLogger(MyRobot.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    public void stateControler() throws Exception
+    {
+        while (true)
+        {
+            switch(stateTable[presentState][statePointer])
+        {
+            case 0: //case A, not bumped
+                presentState = 0;
+                break;
+            case 1: //case B, right bumped
+                presentState = 1;
+                bumpReaction("right");
+                break;
+            case 2: //case C, left bumped
+                presentState = 0;
+                bumpReaction("left");
+                break;
+            case 3: //case D, bump bolth
+                presentState = 0;
+                bumpReaction("both");
+                break;
+                
+        }
+        }
+    }
+   public void bumpReaction(String bumpDirection) throws Exception
+   {
+       if (bumpDirection.equals("right"))
+       {
+           driveDirect(-100, -200);
+       }
+       if (bumpDirection.equals("left"))
+       {
+           driveDirect(-200, -100);
+       }
+       if (bumpDirection.equals("both"))
+       {
+           driveDirect(-100, -100);
+       }
+       SystemClock.sleep(2000);
+       driveDirect(100, 100);
+       statePointer = 0;
+       presentState = 0;
+   }
+   public void checkSensors() throws Exception
+   {
+       readSensors(SENSORS_GROUP_ID6);
+       if ( isBumpRight() && isBumpLeft())
+       {
+           statePointer = 3;    
+       }
+       if (!isBumpRight() && !isBumpLeft())
+       {
+           statePointer = 0;    
+       }
+       if ( isBumpRight() && !isBumpLeft())
+       {
+           statePointer = 1;    
+       }
+       if ( !isBumpRight() && isBumpLeft())
+       {
+           statePointer = 2;    
+       }
+   }
+           
+    
     
 }
